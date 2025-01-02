8c010fd3e6661bfeecbc165f852982c7a4a3af52
virat-18-git <ksprddy6@gmail.com>
2025-01-02 18:28:55 +0530

    converting for into while loop

---
+++ index.py
@@ -0,0 +1,51 @@
+# for i in range(1,11):
+#     for j in range(1,31):
+#         if (i == 7 or i == 8)and j>15:
+#             continue
+#         #  if j==16:
+#         #     break
+        
+#         print(i,j)
+
+
+
+# count=0
+# for i in range(1,11):
+#     for j in range(1,31):
+#         print(i,j)
+#         count+=1
+      
+# print(count)
+
+
+
+# converting for into while loop
+# count=0
+# i=0
+# while i<10:
+#     i+=1
+#     j=1
+#     while j<31:
+#         print(i,j)
+       
+#         j+=1
+
+
+
+count=0
+i=0
+while i<10:
+    print(i)
+    if i==7:
+        i=i+1
+        continue
+    i+=1
+    j=1
+    while j<31:
+        print(i,j)
+       
+        j+=1
+    print(j)
+        
+      
+
